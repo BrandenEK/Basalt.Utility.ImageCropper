03495b838cdc668fda346fce64c6b8fcbcd30145
unknown <brandenek03912@hotmail.com>
2024-08-07 15:56:59 -0400

    core: add code

--- Basalt.Utility.ImageCropper/Program.cs
+++ Basalt.Utility.ImageCropper/Program.cs
@@ -1,9 +1,32 @@
-﻿namespace Basalt.Utility.ImageCropper;
+﻿using System.Drawing;
+
+namespace Basalt.Utility.ImageCropper;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length == 0)
+            throw new Exception("Must pass at least one image path as an argument");
+
+        foreach (string path in args)
+        {
+            Crop(path);
+        }
+    }
+
+    static void Crop(string path)
+    {
+        using Bitmap original = new(path);
+
+        if (original.Width != 4080 || original.Height != 3840)
+            throw new Exception("Original image must be 4080x3840");
+
+        using Bitmap cropped = new(1920, 1080);
+        using Graphics g = Graphics.FromImage(cropped);
+        g.DrawImage(original, 0, -2533);
+
+        original.Dispose();
+        cropped.Save(path);
     }
 }
